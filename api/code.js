7b9b7839bab9861479e7a8413e752e470421e28f
rileyhales <rileyhales1@gmail.com>
2025-05-27 21:14:01 -0600

    don't reparse the json body

--- api/code.js
+++ api/code.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
-  const DISCORD_WEBHOOK_URL = process.env.WEBHOOK
+  const DISCORD_WEBHOOK_URL = process.env.WEBHOOK;
 
-  if (req.method !== 'POST') return res.status(500).json({error: "Invalid Request"})
-  if (!DISCORD_WEBHOOK_URL) return res.status(500).json({error: "Missing webhook URL"})
+  if (req.method !== 'POST') return res.status(500).json({error: "Invalid Request"});
+  if (!DISCORD_WEBHOOK_URL) return res.status(500).json({error: "Missing webhook URL"});
 
-  const body = JSON.parse(req.body);
   let message = `\nIP: ${req.headers['x-real-ip'] || req.connection.remoteAddress}`;
   Object.keys(req.query).forEach((key) => message += `\n${key}: ${req.query[key]}.`);
-  Object.keys(body).forEach((key) => message += `\n${key}: ${body[key]}.`);
-  const payload = JSON.stringify({content: message})
+  Object.keys(req.body).forEach((key) => message += `\n${key}: ${req.body[key]}.`);
+
+  const payload = JSON.stringify({content: message});
 
   try {
     fetch(
